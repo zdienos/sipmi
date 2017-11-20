ffcd03fc0699b1888a94cf1d131453e51083cf1d
mahruszainpcr <mahrus@pcr.ac.id>
2017-11-20 14:26:25 +0700

    ok

--- application/views/indikator/indikator_form.php
+++ application/views/indikator/indikator_form.php
@@ -22,12 +22,12 @@
                       <option value="0">Tidak Ada Indikator</option>
                   </select>
               </div>
-              <div class="hidden" id="indikator">
-
-                 <div class="form-group">
+              <div class="form-group">
                     <label for="varchar">Nama <?php echo form_error('nama') ?></label>
                     <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" value="<?php echo $nama; ?>" />
                 </div>
+                <div class="hidden" id="indikator">
+                 
                 <div class="form-group">
                     <label for="int">Bobot <?php echo form_error('bobot') ?></label>
                     <input type="text" class="form-control" name="bobot" id="bobot" placeholder="Bobot" value="<?php echo $bobot; ?>" />
@@ -43,21 +43,13 @@
                 <div class="form-group">
                     <label for="date">Tgl Mulai <?php echo form_error('tgl_mulai') ?></label>
                     <input type="date" class="form-control" name="tgl_mulai" id="tgl_mulai" placeholder="Tgl Mulai" value="<?php echo $tgl_mulai; ?>" />
+                </div>   
                 </div>
+                 
                 <input type="hidden" name="id_indikator" value="<?php echo $id_indikator; ?>" /> 
                 <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
                 <a href="<?php echo site_url('indikator') ?>" class="btn btn-default">Cancel</a>
-            </div>
-            <div class="hidden" id="tidak_indikator">
-
-             <div class="form-group">
-                <label for="varchar">Nama <?php echo form_error('nama') ?></label>
-                <input type="text" class="form-control" name="nama" id="nama" placeholder="Nama" value="<?php echo $nama; ?>" />
-            </div>
-            <input type="hidden" name="id_indikator" value="<?php echo $id_indikator; ?>" /> 
-            <button type="submit" class="btn btn-primary"><?php echo $button ?></button> 
-            <a href="<?php echo site_url('indikator') ?>" class="btn btn-default">Cancel</a>
-        </div>
+      
         </form><?php $this->load->view('templates/footer');?>
         <script type="text/javascript">
         $(document).ready(function(){
@@ -65,9 +57,7 @@
                 var a=$('#indikator_select').val();
                 if (a==1) {
                     $("#indikator").removeClass('hidden');
-                    $("#tidak_indikator").addClass('hidden');
                 }else{
-                    $("#tidak_indikator").removeClass('hidden');
                     $("#indikator").addClass('hidden');
                 }
              });
